1d059e319835e0080d5426a21045879da3dd20f4
Bashamega <adambashaahmednaji@gmail.com>
2024-08-24 08:41:13 +0300

    Clear

--- dev/README.md
+++
@@ -1,1 +0,0 @@
-This is a react app to perview the lib
--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    transform: {
-      '^.+\\.jsx?$': 'babel-jest',
-      '^.+\\.tsx?$': 'ts-jest',
-    },
-    testEnvironment: 'jsdom',
-    moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
-  };
-  
