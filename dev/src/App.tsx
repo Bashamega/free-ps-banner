330eba75a16aff69e68368c33b48f58536f6d073
Bashamega <adambashaahmednaji@gmail.com>
2024-08-24 06:00:55 +0300

    fix

--- dev/src/App.tsx
+++ dev/src/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import {default as Banner} from 'free-ps-banner'; // Ensure this matches the export
+
 function App() {
   return (
     <main>
-    
+       mvh
+      <Banner title='gjn'/>
     </main>
   );
 }
